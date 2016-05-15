309e42f9a7d4d67c92debdfa200ebd8c57873a1e
PhilB-1 <pbarnett31@gmail.com>
2016-05-14 18:32:09 -0700

    Switch DB from SQLite3 to PG

--- toolbelt/db/schema.rb
+++ toolbelt/db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160514214943) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "flags", force: :cascade do |t|
     t.integer  "flagger_id"
     t.integer  "flagee_id"
