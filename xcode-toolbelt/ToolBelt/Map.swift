cc10af433cfcd301f36b7c2b85729576f367925b
Peter Kang <Peterrkang@gmail.com>
2016-05-14 18:41:36 -0700

    add alamo

--- xcode-toolbelt/ToolBelt/Map.swift
+++ xcode-toolbelt/ToolBelt/Map.swift
@@ -6,8 +6,49 @@
 //  Copyright © 2016 teamToolBelt. All rights reserved.
 //
 
-import Cocoa
+import MapKit
+import Alamofire
+
 
 class Map: UIViewController {
+
+    @IBOutlet var map: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        var latitude:CLLocationDegrees = 43.095181
+        
+        var longitude:CLLocationDegrees = -79.006424
+        
+        var latDelta:CLLocationDegrees = 0.05
+        
+        var longDelta:CLLocationDegrees = 0.05
+        
+        var span: MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
+        
+        var location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        
+        var region: MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        
+        map.setRegion(region, animated: false)
+        
+    }
+    
+    func annotate() {
+        
+        var annotation = MKPointAnnotation()
+        
+        annotation.coordinate = location
+        
+        annotation.title = "Niagra Falls"
+        
+        annotation.subtitle = "One day..."
+        
+        map.addAnnotation(annotation)
+    
+    }
+    
 
 }
